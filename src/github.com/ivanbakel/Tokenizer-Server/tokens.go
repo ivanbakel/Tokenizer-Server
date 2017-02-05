72405d04ee6b2970b1f7c603cae55ec7893b5f65
ivanbakel <ivb@vanbakel.io>
2017-02-05 00:52:45 +0000

    Added individual token endpoint.

--- src/github.com/ivanbakel/Tokenizer-Server/tokens.go
+++ src/github.com/ivanbakel/Tokenizer-Server/tokens.go
@@ -5,18 +5,19 @@ import (
 	"encoding/json"
 	"github.com/ivanbakel/Tokenizer-Server/models"
 	"github.com/vattle/sqlboiler/boil"
+	"github.com/gorilla/mux"
 )
 
 
 func getTokens(w http.ResponseWriter, r *http.Request) {
-	var encoder *json.Encoder = json.NewEncoder(w)
-
 	var tokens,err = models.Tokens(boil.GetDB()).All()
 
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+
+	var encoder *json.Encoder = json.NewEncoder(w)
 
 	for _,token := range tokens {
 		encoder.Encode(token)
@@ -25,7 +26,18 @@ func getTokens(w http.ResponseWriter, r *http.Request) {
 }
 
 func getToken(w http.ResponseWriter, r *http.Request) {
+	var tokenID = mux.Vars(r)["tid"]
 
+	var token,err = models.FindToken(boil.GetDB(), tokenID)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	var encoder *json.Encoder = json.NewEncoder(w)
+
+	encoder.Encode(token)
 }
 
 func giveGroupTokens(w http.ResponseWriter, r *http.Request) {
